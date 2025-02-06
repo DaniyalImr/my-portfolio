b75ea32d36a40491026675890f2252c082ddf2c0
Daniyallmr <dm8751036@gmail.com>
2025-02-06 23:22:38 +0500

    Font

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+//  base: "https://daniyalimr.github.io/my-portfolio/", 
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 1500, // 1000 KB
