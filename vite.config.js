25761c3f141e5083e5f2fe0e465787d0d1a31d8e
Daniyallmr <dm8751036@gmail.com>
2025-02-07 15:15:46 +0500

    App.css

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/",
 //  base: "https://daniyalimr.github.io/my-portfolio/", 
   plugins: [react()],
   build: {
