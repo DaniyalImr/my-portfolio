f56e5383a6960d79f3080cdeb54dece45c064402
Daniyallmr <dm8751036@gmail.com>
2025-02-07 15:02:50 +0500

    again update

--- src/components/heroSection/HeroMain.jsx
+++ src/components/heroSection/HeroMain.jsx
@@ -4,15 +4,13 @@ import DownloadResume from "../DownloadResume";
 
 const HeroMain = () => {
   return (
-    <div className="pt-40 pb-16">
-      <div className="max-w-screen-xl mx-auto px-4">
-        <div className="flex md:flex-row sm:flex-col justify-between items-center relative">
-          <HeroText />
-          <HeroPic />
-        </div>
-        <div className="mt-8 flex justify-center">
-          <DownloadResume />
-        </div>
+    <div className=" pt-40 pb-16">
+      <div className="flex md:flex-row sm:flex-col  max-w-[1200px] mx-auto justify-between items-center relative px-4">
+        <HeroText />
+        <HeroPic />
+      </div>
+      <div>
+        <DownloadResume/>
       </div>
     </div>
   );
